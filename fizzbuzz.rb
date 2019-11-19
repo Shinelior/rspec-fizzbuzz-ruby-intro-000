2a65df5973f72f59d086f0919d3ff8aa4bf5f7d5
lior Shine <shinelior@gmail.com>
2019-11-19 03:53:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,8 +6,8 @@ def fizzbuzz(int)
  if int % 15== 0
    return "Fizz"
  elsif int % 3== 0 
-   return "Buzz"
-elsif  int % 3== 0 
+   return "Fizz"
+elsif  int % 5== 0 
    return "Buzz"
  else
   return nil
